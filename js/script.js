790ef7394dae293ba95862edc902c2a9d20844a6
Amin <polina_rt@mail.ru>
2022-03-23 11:08:28 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const sel = document.querySelector('#sel');
+const inp1 = document.querySelector('#inp1');
+const inp2 = document.querySelector('#inp2');
+const btn = document.querySelector('#btn');
+const out = document.querySelector('#out');
+
+// inp1.style.display = 'none';
+// inp2.style.display = 'none';
+
+// sel.addEventListener('change', (event)=>{
+
+//     if(sel.value == ''){
+//     inp1.style.display = 'none';
+//     inp2.style.display = 'none';
+
+//     }
+
+//     if (sel.value == 1) {
+//        inp1.style.display = 'block';
+//     }
+//     if(sel.value == 2){
+//         inp1.style.display = 'none';
+//         inp2.style.display = 'block';
+//     }
+// });
+
+sel.addEventListener('change', (event)=>{
+
+    if(sel.value == ''){
+        inp1.hidden = true;
+        inp2.hidden = true;
+    }
+    if (sel.value == 1) {
+        inp1.hidden = false;
+        inp2.hidden = true;
+    }
+    if(sel.value == 2){
+        inp1.hidden = true;
+        inp2.hidden = false;
+    }
+});
+
+btn.addEventListener('click',(evrn)=>{
+    let content = inp1.value;
+    out.textContent = content;
+});
