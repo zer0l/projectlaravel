e043531c9f09a2699e0c1a935a84578206dc0f58
NullR0uted <95608807+NullR0uted@users.noreply.github.com>
2022-03-30 11:21:04 +0300

    update

---
+++ cursovya/script/script.js
@@ -0,0 +1,45 @@
+const header = document.querySelector('#header');
+const rad1 = document.querySelector('#rad1');
+const rad2 = document.querySelector('#rad2');
+const rad3 = document.querySelector('#rad3');
+const rad4 = document.querySelector('#rad4');
+
+rad1.addEventListener('change',(event)=>{
+    header.classList.remove('slid1');
+    header.classList.remove('slid2');
+    header.classList.remove('slid3');
+    header.classList.remove('slid4');
+if(rad1.checked){
+    header.classList.toggle('slid1');
+}
+});
+
+rad2.addEventListener('change',(event)=>{
+    header.classList.remove('slid1');
+    header.classList.remove('slid2');
+    header.classList.remove('slid3');
+    header.classList.remove('slid4');
+if(rad2.checked){
+    header.classList.toggle('slid2');
+}
+});
+
+rad3.addEventListener('change',(event)=>{
+    header.classList.remove('slid1');
+    header.classList.remove('slid2');
+    header.classList.remove('slid3');
+    header.classList.remove('slid4');
+if(rad3.checked){
+    header.classList.toggle('slid3');
+}
+});
+
+rad4.addEventListener('change',(event)=>{
+    header.classList.remove('slid1');
+    header.classList.remove('slid2');
+    header.classList.remove('slid3');
+    header.classList.remove('slid4');
+if(rad4.checked){
+    header.classList.toggle('slid4');
+}
+});
